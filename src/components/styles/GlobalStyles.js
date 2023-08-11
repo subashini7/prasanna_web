fc591352c0821e49cd7c92857888a341f0dd77b7
Prasanna Pavani <prasanna@dish.care>
2023-08-11 09:22:07 -0700

    PP 50px

--- src/components/styles/GlobalStyles.js
+++ src/components/styles/GlobalStyles.js
@@ -287,7 +287,8 @@ export const baseStyles = css`
     -moz-box-shadow: -1px 0px 4px 1px rgba(175, 158, 232, 0.8);
     -o-box-shadow: -1px 0px 4px 1px rgba(175, 158, 232, 0.4);
     z-index: 1;
-    padding: 15px;
+    padding: 50px;
+    padding-left: 15px;
     position: relative;
     height: 80px;
   }
